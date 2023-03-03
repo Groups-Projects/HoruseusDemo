78019c43329f1d6f8566849f9e6ed56044567bf4
Groups-Projects <antreasd2@gmail.com>
2023-03-03 14:08:34 +0200

    current progress (except SoulCave and Stylized_Egypt)

---
+++ enc_temp_folder/1969918eede28286e82d96476d746045/CustomPlayerController.cpp
@@ -0,0 +1,66 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CustomPlayerController.h"
+#include "MyCharacter.h"
+
+void ACustomPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	Player = Cast<AMyCharacter>(GetPawn());
+}
+
+void ACustomPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	check(InputComponent);
+	InputComponent->BindAxis("ForwardMovement", this, &ACustomPlayerController::CallForward);
+	InputComponent->BindAxis("StrafeMovement", this, &ACustomPlayerController::CallStrafe);
+	InputComponent->BindAxis("Turning", this, &ACustomPlayerController::CallTurn);
+	InputComponent->BindAxis("LookUp", this, &ACustomPlayerController::CallLookUp);
+	InputComponent->BindAction("Firingz", EInputEvent::IE_Pressed, this, &ACustomPlayerController::CallFire);
+	InputComponent->BindAction("Jumping", EInputEvent::IE_Pressed, this, &ACustomPlayerController::CallJump);
+}
+
+void ACustomPlayerController::CallForward(float Value)
+{
+	if (Player) {
+		Player->MoveForward(Value);
+	}
+}
+
+void ACustomPlayerController::CallStrafe(float Value)
+{
+	if (Player) {
+		Player->MoveStrafe(Value);
+	}
+}
+
+void ACustomPlayerController::CallTurn(float Value)
+{
+	if (Player) {
+		Player->Turn(Value);
+	}
+}
+
+void ACustomPlayerController::CallLookUp(float Value)
+{
+	if (Player) {
+		Player->LookUp(Value);
+	}
+}
+
+void ACustomPlayerController::CallFire()
+{
+	if (Player) {
+		Player->Fire();
+	}
+}
+
+void ACustomPlayerController::CallJump()
+{
+	if (Player) {
+		Player->Jump();
+	}
+}
+
---
+++ enc_temp_folder/3c86a921e2bceddf442dd8caae3c448e/Sceptor.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Sceptor.h"
+
+// Sets default values
+ASceptor::ASceptor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	//WeaponMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Weapon Mesh"));
+	//SetRootComponent(WeaponMesh);
+}
+
+// Called when the game starts or when spawned
+void ASceptor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASceptor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
